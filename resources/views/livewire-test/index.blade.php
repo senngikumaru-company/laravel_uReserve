38565943da715f4d01032d204b2732ee4806e94c
yagi_sotaro <senngikumaru2000@gmail.com>
2023-01-08 21:13:07 +0900

    livewire test

---
+++ resources/views/livewire-test/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        @livewireStyles
+    </head>
+    <body>
+        livewireテスト
+        <div>
+        @if (session()->has('message'))
+            <div class="">
+                {{ session('message') }}
+            </div>
+        @endif
+        </div>
+        {{-- <livewire:counter /> --}}
+        @livewire('counter')
+        @livewireScripts
+    </body>
+</html>
