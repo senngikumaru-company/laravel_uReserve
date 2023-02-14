db9f9e2cd53f91675e36dcc997e36d6c7607e90f
yagi_sotaro <senngikumaru2000@gmail.com>
2023-02-14 14:40:29 +0900

    event dummy 修正

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -29,6 +29,23 @@ class EventService
     {
         $join = $date . " " . $time;
         return Carbon::createFromFormat('Y-m-d H:i', $join);
+    }
 
+    public static function getWeekEvents($startDate , $endDate)
+    {
+        // 定員数を求めるSQLクエリを格納(キャンセルされた人数を除く)
+        $reservedPeople = DB::table('reservations')
+        ->select('event_id', DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull('canceled_date')
+        ->groupBy('event_id');
+
+        return DB::table('events')
+        ->leftJoinSub($reservedPeople, 'reservedPeople',
+        function($join){
+        $join->on('events.id', '=', 'reservedPeople.event_id');
+        })
+        ->whereBetween('events.start_date', [$startDate , $endDate])
+        ->orderBy('events.start_date', 'asc')
+        ->get();
     }
 }
